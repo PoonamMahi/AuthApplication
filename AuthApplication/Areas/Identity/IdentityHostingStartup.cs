8fff31005ca28419b72bac097ad8fadba2691d2a
poonammahi379@gmail.com <poonammahi379@gmail.com>
2022-03-26 02:27:57 +0530

    Add project files.

---
+++ AuthApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AuthApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthApplication.Areas.Identity.IdentityHostingStartup))]
+namespace AuthApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthApplicationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthApplicationContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<AuthApplicationContext>();
+            });
+        }
+    }
+}
---
+++ AuthApplication/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApplication.Controllers
+{
+    [Authorize]
+    public class TestController : Controller
+    {
+        public IActionResult Secret()
+        {
+            return View();
+        }
+    }
+}
